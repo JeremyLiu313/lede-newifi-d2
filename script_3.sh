ebc2f92e1afee7d24e054b65d88eb1b7a78fe2ef
Jeremy Liu <38235044+JeremyLiu313@users.noreply.github.com>
2021-11-17 00:15:13 +0800

    Update script_3.sh

--- script_3.sh
+++ script_3.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-patch -i ${GITHUB_WORKSPACE}/CustomFiles/mac80211_d-team_newifi-d2.patch package/kernel/mac80211/files/lib/wifi/mac80211.sh
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git clone --depth 1 https://github.com/vernesong/OpenClash.git
 
-cp -a ${GITHUB_WORKSPACE}/CustomFiles/system_d-team_newifi-d2 package/base-files/files/etc/config/system
-
-# Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-
-# Modify hostname
-sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
+po2lmo
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
