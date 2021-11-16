0cf4ad64c0e6de3eb9931b30038fbb3a8a3a4d69
Jeremy Liu <38235044+JeremyLiu313@users.noreply.github.com>
2021-11-17 00:15:14 +0800

    Update script_2.sh

--- script_2.sh
+++ script_2.sh
@@ -24,7 +24,7 @@ cd package/luci-app-openclash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git
 
 # po2lmo
-pushd luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
+# pushd luci-app-openclash/tools/po2lmo
+# make && sudo make install
+# popd
 
