0abf556924a4003e468f1491ebf40c1d1ff36b04
Jeremy Liu <38235044+JeremyLiu313@users.noreply.github.com>
2021-11-17 02:29:57 +0800

    Update script_2.sh

--- script_2.sh
+++ script_2.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+
+patch -i ${GITHUB_WORKSPACE}/CustomFiles/mac80211_d-team_newifi-d2.patch package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+cp -a ${GITHUB_WORKSPACE}/CustomFiles/system_d-team_newifi-d2 package/base-files/files/etc/config/system
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
 
 cd package/lean  
 rm -rf luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
 git clone https://github.com/jerrykuku/luci-app-argon-config
 
-cd ../..
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
+cd ..
+mkdir luci-app-openclash
+cd luci-app-openclash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git
 
-# po2lmo
-# pushd luci-app-openclash/tools/po2lmo
-# make && sudo make install
-# popd
+po2lmo
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
 
