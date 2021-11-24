ca8d3cf494df3d0dbec55393310f3618d83055dd
Jeremy Liu <38235044+JeremyLiu313@users.noreply.github.com>
2021-11-24 22:33:11 +0800

    Update script_rpi4.sh

--- script_rpi4.sh
+++ script_rpi4.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 cd package/lean  
 rm -rf luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
 git clone https://github.com/jerrykuku/luci-app-argon-config
 
-cd ../..
-git clone  https://github.com/bigbugcc/OpenwrtApp package/OpenwrtApp
+cd ..
+git clone  https://github.com/bigbugcc/OpenwrtApp
 
-cd package
-mkdir luci-app-openclash
-cd luci-app-openclash
-git clone --depth 1 https://github.com/vernesong/OpenClash.git
+# mkdir luci-app-openclash
+# cd luci-app-openclash
+# git clone --depth 1 https://github.com/vernesong/OpenClash.git
 
 # po2lmo
 # pushd luci-app-openclash/tools/po2lmo
